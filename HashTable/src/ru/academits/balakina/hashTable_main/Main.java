b27f16cea5a3a28c213417e7c5b1e1a1f99ce031
vera-ПК\vera <e.p.balakina@mail.ru>
2020-08-18 23:05:30 +0700

    Задача HashTable, первый вариант решения

---
+++ HashTable/src/ru/academits/balakina/hashTable_main/Main.java
@@ -0,0 +1,79 @@
+package ru.academits.balakina.hashTable_main;
+
+import ru.academits.balakina.hashTable.HashTable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        HashTable<Integer> hashTable = new HashTable<>();
+
+        System.out.println(hashTable);
+        System.out.println("size = " + hashTable.size());
+        System.out.println();
+
+        System.out.println("Добавим 5 в хэш-таблицу: " + hashTable.add(5));
+        System.out.println(hashTable);
+        System.out.println("size = " + hashTable.size());
+        System.out.println();
+
+        System.out.println("Добавим 10 в хэш-таблицу: " + hashTable.add(10));
+        System.out.println(hashTable);
+        System.out.println("size = " + hashTable.size());
+        System.out.println();
+
+        System.out.println("Добавим 20 в хэш-таблицу: " + hashTable.add(20));
+        System.out.println(hashTable);
+        System.out.println("size = " + hashTable.size());
+        System.out.println();
+
+        System.out.println("Хэш-таблица содержит 4? -  " + hashTable.contains(4));
+        System.out.println("Хэш-таблица содержит 5? -  " + hashTable.contains(5));
+        System.out.println("Хэш-таблица содержит 10? -  " + hashTable.contains(10));
+        System.out.println();
+
+        System.out.println("Удалим 20: " + hashTable.remove(20));
+        System.out.println(hashTable);
+        System.out.println("size = " + hashTable.size());
+        System.out.println();
+
+        // список для проверки containsAll
+        ArrayList<Integer> arrayToCheck = new ArrayList<>();
+        arrayToCheck.add(5);
+        arrayToCheck.add(10);
+        System.out.println("Хэш-таблица содержит [5, 10]? -  " + hashTable.containsAll(arrayToCheck));
+
+        arrayToCheck.add(20);
+        System.out.println("Хэш-таблица содержит [5, 10, 20]? -  " + hashTable.containsAll(arrayToCheck));
+
+        System.out.println("Добавим в хэш-таблицу [5, 10, 20]: " + hashTable.addAll(arrayToCheck));
+        System.out.println(hashTable);
+        System.out.println("size = " + hashTable.size());
+        System.out.println();
+
+        System.out.println("Добавим в хэш-таблицу [7, 4, 2, 3]: " + hashTable.addAll(new ArrayList<>(Arrays.asList(7, 4, 2, 3))));
+        System.out.println(hashTable);
+        System.out.println("size = " + hashTable.size());
+        System.out.println();
+
+        System.out.println("Удалим из хэш-таблицы [5, 10, 20]: " + hashTable.removeAll(arrayToCheck));
+        System.out.println(hashTable);
+        System.out.println("size = " + hashTable.size());
+        System.out.println();
+
+        // retainAll
+        System.out.println("Оставим в хэш-таблице только [2, 3]: " + hashTable.retainAll(new ArrayList<>(Arrays.asList(2, 3))));
+        System.out.println(hashTable);
+        System.out.println("size = " + hashTable.size());
+        System.out.println();
+
+
+        /*System.out.println("Очистим хэш-таблицу");
+        hashTable.clear();
+        System.out.println(hashTable);
+        System.out.println("size = " + hashTable.size());
+        System.out.println();*/
+
+    }
+}
